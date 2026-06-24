b5803f8511f46324fd3107508f233ccbc635f577
rahulkpr2510 <rahulkpr2510@gmail.com>
2026-06-24 12:45:57 +0530

    build: update dependencies and next/vercel configurations

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [{ protocol: "https", hostname: "ik.imagekit.io" }],
+  },
+  serverExternalPackages: ["playwright", "@sparticuz/chromium"],
 };
 
 export default nextConfig;
